aec63ea5c4341203c84be24d0edcbd4127f91d65
Aaron McDonald <aaronmcdonald@macbookair.mynetworksettings.com>
2025-07-31 10:46:03 -0400

    Added the building blocks to the clean-code.js file mainly have resources linked on the file and brief descriptions

---
+++ clean-code.js
@@ -0,0 +1,17 @@
+// This file documents efficient coding practices top enhance
+// readibility and have cleaner more descriptive code
+
+// read more at these links:
+
+
+/*
+
+- The Odin Project | Clean Code Lesson: https://www.theodinproject.com/lessons/foundations-clean-code
+
+- 10 Principles for Keeping Your Programming Code Clean: https://onextrapixel.com/10-principles-for-keeping-your-programming-code-clean/
+
+- Code Tells You How, Comments Tell You Why: https://blog.codinghorror.com/code-tells-you-how-comments-tell-you-why/
+
+- Coding Without Comments: https://blog.codinghorror.com/coding-without-comments/
+
+*/
