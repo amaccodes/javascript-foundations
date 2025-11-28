a13116db4783bfe45ef023fde84f7a947fb58ec1
Aaron McDonald <aaronmcd1025@gmail.com>
2025-11-28 17:56:08 -0500

    update

--- full-stack-javascript-course/webpack-practice/src/index.js
+++ full-stack-javascript-course/webpack-practice/src/index.js
@@ -1,3 +1,6 @@
 import {greeting} from "./greeting.js"
+import './style.css'
+// we import the css stylesheet because we use css-loader package to process stylesheet and 
+// then style-loader to transform it into javascript code. Therefore we do not need to tag it in the html file.
 
 console.log(greeting)
